6e41e8aca9ce046826e616a695aff621f4b5fceb
Lucas Machado <lmachado72@outlook.com>
2026-05-21 02:29:28 -0300

    fix(sentry): changing from .env.node_env to .env.sentry_dsn

--- apps/api/src/instrument.ts
+++ apps/api/src/instrument.ts
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/hono/node";
 
 Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    enabled: process.env.NODE_ENV === "production",
+    enabled: !!process.env.SENTRY_DSN,
     environment: process.env.NODE_ENV ?? "development",
     tracesSampleRate: 0.1,
 });
